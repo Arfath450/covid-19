5868573c4ca90d2d82719f0d2ab2eb07b8ba3bc3
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-07-12 20:26:16 +0530

    Updates

---
+++ precache-manifest.e597d22f0f93f6d88082742c64671b10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b0a1b8d29b3bfe6fa9d7b6b0661241",
+    "url": "/covid-status/index.html"
+  },
+  {
+    "revision": "e2e84e4da3076d3df105",
+    "url": "/covid-status/static/css/main.973be1e2.chunk.css"
+  },
+  {
+    "revision": "d14b39604a9d3ee849f0",
+    "url": "/covid-status/static/js/2.ab9a9b43.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/covid-status/static/js/2.ab9a9b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e84e4da3076d3df105",
+    "url": "/covid-status/static/js/main.7c674908.chunk.js"
+  },
+  {
+    "revision": "dc46bd5dae50cf9bc6ba",
+    "url": "/covid-status/static/js/runtime-main.d6d017be.js"
+  }
+]);
